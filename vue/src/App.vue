edccc226490496c1fc0fe85723cb3243a19371b2
bookstore <booookstore@icloud.com>
2018-03-04 20:00:13 +0900

    feat : Implement User component and table

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-header>
+      <el-menu :default-active="'1'" mode="horizontal">
+        <el-menu-item index="1"><router-link to="/user">Users</router-link></el-menu-item>
+        <el-menu-item index="2">Animal</el-menu-item>
+        <el-menu-item index="3">Area</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
   </div>
 </template>
 
@@ -16,8 +24,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ vue/src/components/User.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <el-table
+      :data="userData"
+      style="width: 100%">
+      <el-table-column
+        prop="ID"
+        label="ID"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="Name"
+        label="Name"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="Age"
+        label="Age"
+        width="180">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'User',
+  data () {
+    return {
+      msg: 'User',
+      userData: [{
+        ID: 1,
+        Name: 'BookStore',
+        Age: 24
+      }, {
+        ID: 2,
+        Name: 'BookStore',
+        Age: 24
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
